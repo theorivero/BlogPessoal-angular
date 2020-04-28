fe1b70c38d364fc218cd633103baa663ffe2a039
theorivero <thrivero1@gmail.com>
2020-04-28 19:38:14 -0300

    service com get

--- blogpessoal/src/app/service/postagem.service.ts
+++ blogpessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,16 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllpostagens(){
+    return this.http.get('http://31.220.57.14:8000/postagens')
+  }
+
 }
